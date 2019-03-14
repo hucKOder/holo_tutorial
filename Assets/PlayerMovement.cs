8ff6c98232f3457045c565c0d57c07e2c55f8144
Andrej Hucko <hucko.andrej.mail@gmail.com>
2019-03-14 11:13:53 +0100

    First commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideWayForce = 500f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideWayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWayForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
